788addddea95a9d30f4348e0c917dc8f9fb8b9fd
Trần Tâm <89630189+miniblockexe@users.noreply.github.com>
2026-03-25 10:33:00 +0700

    fix file path

--- run.bat
+++ run.bat
@@ -1,9 +1,11 @@
+cd /d %~dp0
+
 start cmd /k "cd StudentService && dotnet run"
 start cmd /k "cd TeacherService && dotnet run"
 start cmd /k "cd ClassService && dotnet run"
 
 timeout /t 5
 
-start http://localhost:3456/swagger
-start http://localhost:5678/swagger
-start http://localhost:6789/swagger
+start "" "http://localhost:3456/swagger"
+start "" "http://localhost:5678/swagger"
+start "" "http://localhost:6789/swagger"
